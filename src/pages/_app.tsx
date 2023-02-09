e7b026c575c3b12c973d352d4f6c4e57cc3eb395
Philipp Dyck <pdyck@posteo.de>
2023-02-09 20:41:31 +0100

    Connect Supabase Authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { AppProps } from "next/app";
+import { useState } from "react";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    const [supabase] = useState(() => createBrowserSupabaseClient());
+
+    return (
+        <SessionContextProvider
+            supabaseClient={supabase}
+            initialSession={pageProps.initialSession}
+        >
+            <Component {...pageProps} />
+        </SessionContextProvider>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
+import { useSession, useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { Auth, ThemeMinimal } from "@supabase/auth-ui-react";
 import Head from "next/head";
 
 export default function Home() {
+    const session = useSession();
+    const supabase = useSupabaseClient();
+
+    const user = useUser();
+
     return (
         <>
             <Head>
@@ -8,7 +15,15 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1>Short Link Management</h1>
+            {!session ? (
+                <Auth supabaseClient={supabase} appearance={{ theme: ThemeMinimal }} />
+            ) : (
+                <>
+                    <h1>Short Link Management</h1>
+                    <h2>Hello, {user?.email}</h2>
+                    <button onClick={() => supabase.auth.signOut()}>Sign out</button>
+                </>
+            )}
         </>
     );
 }
